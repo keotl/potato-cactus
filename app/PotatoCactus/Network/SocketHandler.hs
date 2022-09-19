cf95ba9539c79c90cd5b8d4a989b805e3550c676
Kento Lauzon <kento.lauzon@ligature.ca>
2022-09-19 14:13:33 +0100

    join dispatch log unknown opcode

--- app/PotatoCactus/Network/SocketHandler.hs
+++ app/PotatoCactus/Network/SocketHandler.hs
@@ -9,25 +9,27 @@ import Data.Binary.Strict.Get
 import qualified Data.ByteString as S
 import Data.ByteString.Builder (byteStringHex)
 import Data.ByteString.UTF8 as BSU
+import Data.Either
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
 import PotatoCactus.Login.LoginHandler (handleLogin)
-import Data.Either
 
 socketMain :: Socket -> IO ()
 socketMain sock = do
   msg <- recv sock 1
   -- let decoded =
   case runGet getWord8 msg of
-        (Right val, _) -> dispatch val sock
-        (Left val, _) -> return ()
-        
+    (Right val, _) -> dispatch val sock
+    (Left val, _) -> return ()
+
 dispatch :: B.Word8 -> Socket -> IO ()
 dispatch 14 = handleLogin -- actual login
 dispatch 48 = handleLogin -- '0'
-dispatch op = do
-  print -- todo how to actually print op??
+dispatch op = \x -> do
+  putStrLn $ "Unknown opcode: " ++ show op
+  print x
 
+-- todo how to actually print op??
 
 socketMain2 :: Socket -> IO ()
 socketMain2 sock = do
