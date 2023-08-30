7adc1bafa25c39c4c96cb7df494ee3e6e7420336
Kento Lauzon <kento@lauzon.co.uk>
2023-08-30 19:31:36 +0100

    attempt to include static set logic in dynamic object collection

--- app/PotatoCactus/Game/Entity/Object/DynamicObjectCollection.hs
+++ app/PotatoCactus/Game/Entity/Object/DynamicObjectCollection.hs
@@ -3,11 +3,13 @@ module PotatoCactus.Game.Entity.Object.DynamicObjectCollection where
 import Data.IntMap (IntMap, alter, delete, empty, findWithDefault, toList)
 import Data.IntMap.Lazy (insert)
 import Data.Maybe (fromMaybe)
+import Debug.Trace (trace)
 import PotatoCactus.Config.Constants (chunkBoundExponentXY_)
 import PotatoCactus.Game.Definitions.Types.GameObjectDefinition (GameObjectId)
 import PotatoCactus.Game.Entity.Object.GameObject (GameObject (id, objectType, position), GameObjectType, gameObjectHash)
 import PotatoCactus.Game.Position (GetPosition (getPosition), Position (Position, x, y, z), chunkX, chunkY)
 import qualified PotatoCactus.Game.Position as Pos
+import PotatoCactus.Utils.Flow ((|>))
 import Prelude hiding (id)
 
 data DynamicObject = Added GameObject | Removed GameObject deriving (Eq, Show)
@@ -24,67 +26,81 @@ data DynamicObjectCollection = DynamicObjectCollection
 create :: DynamicObjectCollection
 create = DynamicObjectCollection empty
 
-addDynamicObject :: GameObject -> DynamicObjectCollection -> DynamicObjectCollection
-addDynamicObject object collection =
+addDynamicObject :: (Position -> GameObjectType -> [GameObject]) -> GameObject -> DynamicObjectCollection -> DynamicObjectCollection
+addDynamicObject staticObjectAt object collection =
   let updatedElements =
         alter
-          (addObjectInChunkMap_ object)
+          (addObjectInChunkMap_ object (staticObjectAt (getPosition object) (objectType object)))
           (objChunkKey_ object)
           (elements_ collection)
    in collection {elements_ = updatedElements}
 
-addObjectInChunkMap_ :: GameObject -> Maybe (IntMap [DynamicObject]) -> Maybe (IntMap [DynamicObject])
-addObjectInChunkMap_ obj chunkMap =
+addObjectInChunkMap_ :: GameObject -> [GameObject] -> Maybe (IntMap [DynamicObject]) -> Maybe (IntMap [DynamicObject])
+addObjectInChunkMap_ obj matchingStaticObjects chunkMap =
   Just $
     alter
-      (addObjectToTile_ obj)
+      (addObjectToTile_ obj matchingStaticObjects)
       (key_ obj)
       (fromMaybe empty chunkMap)
 
-addObjectToTile_ :: GameObject -> Maybe [DynamicObject] -> Maybe [DynamicObject]
-addObjectToTile_ newObject Nothing = Just [Added newObject]
-addObjectToTile_ newObject (Just tileObjects) =
+addObjectToTile_ :: GameObject -> [GameObject] -> Maybe [DynamicObject] -> Maybe [DynamicObject]
+addObjectToTile_ newObject matchingStaticObjects Nothing = Just $ Added newObject : map Removed matchingStaticObjects
+addObjectToTile_ newObject matchingStaticObjects (Just tileObjects) =
   case matchingEntry_ newObject tileObjects of
-    Nothing -> Just $ Added newObject : tileObjects
-    Just existing -> case existing of
-      Added _ -> Just tileObjects
+    Nothing -> Just $ Added newObject : tileObjects ++ map Removed matchingStaticObjects
+    (Just existing) -> case existing of
+      Added replaced -> Just $ Added newObject : filter ((/=) replaced . unwrap_) tileObjects
       Removed _ -> Just $ filter ((/=) newObject . unwrap_) tileObjects
 
 matchingEntry_ :: GameObject -> [DynamicObject] -> Maybe DynamicObject
 matchingEntry_ _ [] = Nothing
 matchingEntry_ predicateObject (x : xs) =
   let object = unwrap_ x
-   in if predicateObject == object
+   in if objectType predicateObject == objectType object
         then Just x
         else matchingEntry_ predicateObject xs
 
-removeDynamicObject :: GameObject -> DynamicObjectCollection -> DynamicObjectCollection
-removeDynamicObject object collection =
-  let updated =
-        alter
-          (removeObjectFromChunkMap_ object)
-          (objChunkKey_ object)
-          (elements_ collection)
-   in collection {elements_ = updated}
+removeDynamicObject :: (Position -> GameObjectType -> [GameObject]) -> GameObject -> DynamicObjectCollection -> DynamicObjectCollection
+removeDynamicObject staticObjectAt object collection =
+  let matchingStaticObjects =
+        staticObjectAt (getPosition object) (objectType object)
+          |> filter ((==) (id object) . id)
+   in let updated =
+            alter
+              (removeObjectFromChunkMap_ object matchingStaticObjects)
+              (objChunkKey_ object)
+              (elements_ collection)
+       in collection {elements_ = updated}
 
-removeObjectFromChunkMap_ :: GameObject -> Maybe (IntMap [DynamicObject]) -> Maybe (IntMap [DynamicObject])
-removeObjectFromChunkMap_ object Nothing =
+removeObjectFromChunkMap_ :: GameObject -> [GameObject] -> Maybe (IntMap [DynamicObject]) -> Maybe (IntMap [DynamicObject])
+removeObjectFromChunkMap_ _ [] Nothing = Nothing
+removeObjectFromChunkMap_ object (matchingStaticObject : _) Nothing =
   Just $
     insert (key_ object) [Removed object] empty
-removeObjectFromChunkMap_ object (Just chunkMap) =
+removeObjectFromChunkMap_ object staticObjects (Just chunkMap) =
   Just $
     alter
-      (removeFromTileObjects_ object)
+      (removeFromTileObjects_ object staticObjects)
       (key_ object)
       chunkMap
 
-removeFromTileObjects_ :: GameObject -> Maybe [DynamicObject] -> Maybe [DynamicObject]
-removeFromTileObjects_ removed Nothing = Just [Removed removed]
-removeFromTileObjects_ removed (Just tileObjects) =
-  case matchingEntry_ removed tileObjects of
-    Nothing -> Just $ Removed removed : tileObjects
-    Just existing -> case existing of
-      Added _ -> Just $ filter ((/=) removed . unwrap_) tileObjects
+removeFromTileObjects_ :: GameObject -> [GameObject] -> Maybe [DynamicObject] -> Maybe [DynamicObject]
+removeFromTileObjects_ _ [] Nothing = Nothing
+removeFromTileObjects_ removed (matchingStaticObject : _) Nothing = Just [Removed removed]
+removeFromTileObjects_ removed staticObjects (Just tileObjects) =
+  case (matchingEntry_ removed tileObjects, staticObjects) of
+    (Nothing, []) -> Just tileObjects
+    (Nothing, _) -> Just $ Removed removed : tileObjects
+    (Just existing, []) -> case existing of
+      Added _ -> Just (tileObjects |> filter ((/=) removed . unwrap_))
+      Removed _ -> Just tileObjects
+    (Just existing, matchingStaticObject : _) -> case existing of
+      Added _ ->
+        Just
+          ( tileObjects
+              |> filter ((/=) removed . unwrap_)
+              |> filter ((/=) matchingStaticObject . unwrap_)
+          )
       Removed _ -> Just tileObjects
 
 key_ :: GameObject -> Int
--- tests/Game/DynamicObjectCollectionTests.hs
+++ tests/Game/DynamicObjectCollectionTests.hs
@@ -1,8 +1,9 @@
 module Game.DynamicObjectCollectionTests where
 
+import Debug.Trace (trace)
 import PotatoCactus.Game.Entity.Object.DynamicObjectCollection (DynamicObject (Added, Removed), DynamicObjectCollection)
 import qualified PotatoCactus.Game.Entity.Object.DynamicObjectCollection as ObjectCollection
-import PotatoCactus.Game.Entity.Object.GameObject (GameObject (GameObject))
+import PotatoCactus.Game.Entity.Object.GameObject (GameObject (GameObject), GameObjectType)
 import PotatoCactus.Game.Position (GetPosition (getPosition), Position (Position), chunkX, chunkY)
 import PotatoCactus.Utils.Flow ((|>))
 import Test.HUnit
@@ -13,22 +14,33 @@ testDynamicObjectCollection =
         ( assertEqual
             "add brand new object should create Added dynamic object"
             [Added obj]
-            (ObjectCollection.iter (ObjectCollection.addDynamicObject obj ObjectCollection.create))
+            (ObjectCollection.iter (ObjectCollection.addDynamicObject dummyStaticSetLookup obj ObjectCollection.create))
         ),
       TestCase
         ( assertEqual
             "remove Added object"
             []
             ( ObjectCollection.iter $
-                ObjectCollection.removeDynamicObject obj collection
+                ObjectCollection.removeDynamicObject dummyStaticSetLookup obj collection
             )
         ),
       TestCase
         ( assertEqual
-            "removing a brand new object should add as Removed"
-            [Removed obj]
+            "removing an object not in the static set should do nothing"
+            []
             ( ObjectCollection.iter $
-                ObjectCollection.removeDynamicObject obj ObjectCollection.create
+                ObjectCollection.removeDynamicObject dummyStaticSetLookup obj ObjectCollection.create
+            )
+        ),
+      TestCase
+        ( assertEqual
+            "add over another dynamic object replaces in-place"
+            [Added obj]
+            ( ObjectCollection.iter
+                ( ObjectCollection.create
+                    |> ObjectCollection.addDynamicObject dummyStaticSetLookup staticObj
+                    |> ObjectCollection.addDynamicObject dummyStaticSetLookup obj
+                )
             )
         ),
       TestCase
@@ -37,9 +49,10 @@ testDynamicObjectCollection =
             []
             ( ObjectCollection.iter
                 ( ObjectCollection.removeDynamicObject
+                    (\_ _ -> [obj])
                     obj
                     ObjectCollection.create
-                    |> ObjectCollection.addDynamicObject obj
+                    |> ObjectCollection.addDynamicObject dummyStaticSetLookup obj
                 )
             )
         ),
@@ -48,15 +61,65 @@ testDynamicObjectCollection =
             "findByChunkXY"
             [Added obj]
             (ObjectCollection.findByChunkXY (chunkX . getPosition $ obj) (chunkY . getPosition $ obj) 0 collection)
+        ),
+      TestCase
+        ( assertEqual
+            "addDynamicObject with replaced object in static set, should mark static object as removed"
+            [Added obj, Removed staticObj]
+            ( ObjectCollection.iter
+                ( ObjectCollection.create
+                    |> ObjectCollection.addDynamicObject (\_ _ -> [staticObj]) obj
+                )
+            )
+        ),
+      TestCase
+        ( assertEqual
+            "re-adding a replaced static object resets to initial state"
+            []
+            ( ObjectCollection.iter
+                ( ObjectCollection.create
+                    |> ObjectCollection.addDynamicObject (\_ _ -> [staticObj]) obj
+                    |> ObjectCollection.addDynamicObject (\_ _ -> [staticObj]) staticObj
+                )
+            )
+        ),
+      TestCase
+        ( assertEqual
+            "addDynamicObject with replaced object in static set AND existing chunkmap, should mark static object as removed"
+            [Added obj, Removed staticObj]
+            ( ObjectCollection.iter
+                ( ObjectCollection.create
+                    |> ObjectCollection.addDynamicObject dummyStaticSetLookup obj
+                    |> ObjectCollection.removeDynamicObject dummyStaticSetLookup obj
+                    |> ObjectCollection.addDynamicObject (\_ _ -> [staticObj]) obj
+                )
+            )
+        ),
+      TestCase
+        ( assertEqual
+            "removeDynamicObject on an Added object replacing a static object, should remove both the Added and Removed objects"
+            []
+            ( ObjectCollection.iter
+                ( ObjectCollection.create
+                    |> ObjectCollection.addDynamicObject (\_ _ -> [staticObj]) obj
+                    |> ObjectCollection.removeDynamicObject (\_ _ -> [staticObj]) obj
+                )
+            )
         )
     ]
 
 collection :: DynamicObjectCollection
 collection =
   foldl
-    (flip ObjectCollection.addDynamicObject)
+    (flip (ObjectCollection.addDynamicObject dummyStaticSetLookup))
     ObjectCollection.create
     [obj]
 
 obj :: GameObject
 obj = GameObject 123 (Position 3167 3304 0) 0 0
+
+staticObj :: GameObject
+staticObj = GameObject 456 (Position 3167 3304 0) 0 0
+
+dummyStaticSetLookup :: Position -> GameObjectType -> [GameObject]
+dummyStaticSetLookup _ _ = []
